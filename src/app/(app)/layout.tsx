3fedd240a9926d5f7075d84e63e7391063fe083e
Prabal-verma <122899996+Prabal-verma@users.noreply.github.com>
2024-06-16 04:31:42 +0530

    dashboard and u page added

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import './globals.css';
 import { Toaster } from "@/components/ui/toaster"
-import AuthProvider from '../context/AuthProvider';
+import AuthProvider from '../../context/AuthProvider';
 
 const inter = Inter({ subsets: ['latin'] });
 
