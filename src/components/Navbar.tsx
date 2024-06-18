86e17732aaaf5f9808b0512266465fff03820a24
Prabal-verma <122899996+Prabal-verma@users.noreply.github.com>
2024-06-19 03:19:31 +0530

    redirect to home after logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,11 +5,15 @@ import Link from 'next/link';
 import { useSession, signOut } from 'next-auth/react';
 import { Button } from './ui/button';
 import { User } from 'next-auth';
+import { link } from 'fs';
 
 function Navbar() {
   const { data: session } = useSession();
   const user : User = session?.user;
-
+  const handleSignOut = async () => {
+    await signOut({ redirect: false }); // Use { redirect: false } to prevent NextAuth default redirect behavior
+    window.location.href = '/'; // Redirect manually to homepage
+  };
   return (
     <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
@@ -21,7 +25,7 @@ function Navbar() {
             <span className="mr-4">
               Welcome, {user.username || user.email}
             </span>
-            <Button onClick={() => signOut()} className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>
+            <Button onClick={handleSignOut} className="w-full md:w-auto bg-slate-100 text-black" variant='outline'>
               Logout
             </Button>
           </>
